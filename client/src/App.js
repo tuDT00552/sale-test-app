b8bc638483123145f1f240c2e68bd7414ca3e4ef
tudt <tudt@itsol.vn>
2021-03-17 16:47:41 +0700

    temp

--- client/src/App.js
+++ client/src/App.js
@@ -20,32 +20,36 @@ function App() {
 
   const renderProduct = product => {
     return (
-      <div style={{ width: '20%', float: 'left' }} key={product._id} className="list__item product">
-        <a style={{ ['text-decoration']: 'none', ['font-weight']: 'bold', ['text-align']: 'center', color: 'black' }} target="_blank" href={"https://shopee.vn/" + product.name + "-i." + product.shopid + "." + product.itemid}>
-          <div style={{ padding: '10px' }}>
-            <img src={"https://cf.shopee.vn/file/" + product.image} width="100%"></img>
-            {/* <img src="https://kb.rspca.org.au/wp-content/uploads/2018/11/golder-retriever-puppy.jpeg" width="100%"></img> */}
-            <strong style={{ ['font-size']: '9px' }} className="product__price">{product.price.toString().substring(0, 4)} VND</strong>
-          </div>
-          <div style={{
-            ['font-size']: '10px', padding: '5px', ['white-space']: 'nowrap',
-            width: '90%',
-            overflow: 'hidden',
-            ['text-overflow']: 'ellipsis',
-            padding: '5px'
-          }} className="product__name">{product.name}</div>
-        </a>
-      </div >
+      <div>
+        <div style={{ width: '50%', float: 'left' }} key={product._id} className="list__item product">
+          <a style={{ ['text-decoration']: 'none', ['font-weight']: 'bold', ['text-align']: 'center', color: 'black' }} target="_blank" href={"https://shopee.vn/" + product.name + "-i." + product.shopid + "." + product.itemid}>
+            <div style={{ padding: '10px' }}>
+              <img src={"https://cf.shopee.vn/file/" + product.image} width="100%"></img>
+              {/* <img src="https://kb.rspca.org.au/wp-content/uploads/2018/11/golder-retriever-puppy.jpeg" width="100%"></img> */}
+              <strong style={{ ['font-size']: '9px' }} className="product__price">{product.price.toString().substring(0, 4)} VND</strong>
+            </div>
+            <div style={{
+              ['font-size']: '10px', padding: '5px', ['white-space']: 'nowrap',
+              width: '90%',
+              overflow: 'hidden',
+              ['text-overflow']: 'ellipsis',
+              padding: '5px'
+            }} className="product__name">{product.name}</div>
+          </a>
+        </div >
+      </div>
     );
   };
 
   return (
     <div className="App">
       <ul className="list">
+        <h1>Xốp Be 1 Ngìn™</h1><spam>Version 1 dang update ...</spam>
+        <hr></hr>
         {(products && products.length > 0) ? (
           products.map(product => renderProduct(product))
         ) : (
-          <p>Chua hien thi san pham, vui long cho .....</p>
+          <p>Chua hien thi san pham, cho ti nke .....</p>
         )}
       </ul>
     </div>
--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -4,6 +4,6 @@ export default {
   getAll: async () => {
     let res = await axios.get(`/api/sale/705977/0`);
     return res.data.filter(item => item.price === 100000000) || [];
-    // return res.data || [];                     
+    return res.data || [];                     
   }
 }
