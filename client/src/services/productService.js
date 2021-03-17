08887c2c4f4980db06dd88b9a04f8d54b27564c8
tudt <tudt@itsol.vn>
2021-03-17 16:38:41 +0700

    temp

--- client/src/services/productService.js
+++ client/src/services/productService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default {
   getAll: async () => {
     let res = await axios.get(`/api/sale/705977/0`);
-    // return res.data.filter(item => item.price === 100000000) || [];
-    return res.data || [];                     
+    return res.data.filter(item => item.price === 100000000) || [];
+    // return res.data || [];                     
   }
 }
