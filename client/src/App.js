8657339e77c2077ead4b90b21d5cc86ec78d243d
tudt <tudt@itsol.vn>
2021-03-17 16:35:43 +0700

    temp

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ function App() {
   const renderProduct = product => {
     return (
       <div style={{ width: '20%', float: 'left' }} key={product._id} className="list__item product">
-        <a style={{ ['text-decoration']: 'none', ['font-weight']: 'bold', ['text-align']: 'center' }} target="_blank" href={"https://shopee.vn/" + product.name + "-i." + product.shopid + "." + product.itemid}>
+        <a style={{ ['text-decoration']: 'none', ['font-weight']: 'bold', ['text-align']: 'center', color: 'black' }} target="_blank" href={"https://shopee.vn/" + product.name + "-i." + product.shopid + "." + product.itemid}>
           <div style={{ padding: '10px' }}>
             <img src={"https://cf.shopee.vn/file/" + product.image} width="100%"></img>
             {/* <img src="https://kb.rspca.org.au/wp-content/uploads/2018/11/golder-retriever-puppy.jpeg" width="100%"></img> */}
